93fb9d3d3c2269fb8b40f12f6c2a2107f498fb27
NodgeTheRage <massacrebakuto@gmail.com>
2020-01-07 17:20:04 +0300

    Вторая часть задания

--- source/js/test.js
+++ source/js/test.js
@@ -4,7 +4,6 @@ var header = document.querySelector(".page-header");
 
 document.addEventListener("DOMContentLoaded", function(){
     menuShow.classList.toggle("nav-list--show");
-    burger.classList.toggle("page-nav__toggle--cross");
     header.classList.toggle("page-header--menu");
     header.classList.toggle("page-header--inactive");
 });
