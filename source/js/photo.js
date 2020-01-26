d54fe4e000f89c294d7662a9e57c1ffd847ef141
NodgeTheRage <massacrebakuto@gmail.com>
2020-01-26 14:09:25 +0300

    На защиту

--- source/js/photo.js
+++ source/js/photo.js
@@ -2,7 +2,6 @@ var filterIconFirst = document.querySelector(".post__icon--1");
 var filterIconSecond = document.querySelector(".post__icon--2");
 var filterIconThird = document.querySelector(".post__icon--3");
 
-
 filterIconFirst.addEventListener("click", function (evt) {
   filterIconFirst.classList.add("post__icon--active");
   filterIconSecond.classList.remove("post__icon--active");
--- source/js/test.js
+++ source/js/test.js
@@ -5,14 +5,15 @@ var toggleBurger = document.querySelector(".page-nav__svg--burger");
 var toggleCross = document.querySelector(".page-nav__svg--cross");
 
 document.addEventListener("DOMContentLoaded", function(){
-    menuShow.classList.toggle("nav-list--show");
-    header.classList.toggle("page-header--menu");
-    header.classList.toggle("page-header--inactive");
+  menuShow.classList.toggle("nav-list--show");
+  header.classList.toggle("page-header--menu");
+  header.classList.toggle("page-header--inactive");
+  navButton.classList.remove("page-nav__toggle--hide");
 });
 navButton.addEventListener("click", function (evt) {
-    menuShow.classList.toggle("nav-list--show");
-    header.classList.toggle("page-header--menu");
-    header.classList.toggle("page-header--inactive");
-    toggleCross.classList.toggle("page-nav__svg--hidden");
-    toggleBurger.classList.toggle("page-nav__svg--hidden");
+  menuShow.classList.toggle("nav-list--show");
+  header.classList.toggle("page-header--menu");
+  header.classList.toggle("page-header--inactive");
+  toggleCross.classList.toggle("page-nav__svg--hidden");
+  toggleBurger.classList.toggle("page-nav__svg--hidden");
 });
